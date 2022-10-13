48a0ae92e7a775924ac0543a0f91311522392647
crowelian <harriahola@live.fi>
2022-10-13 18:29:23 +0300

    Initial Commit

---
+++ JadeGo/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace JadeGo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.allowHttpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectSearchEngineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.googleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.duckDuckGoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.go = new System.Windows.Forms.Button();
+            this.webViewMain = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.back = new System.Windows.Forms.Button();
+            this.refresh = new System.Windows.Forms.Button();
+            this.forward = new System.Windows.Forms.Button();
+            this.Reload_Anim_label = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webViewMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(944, 9);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(31, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.allowHttpToolStripMenuItem,
+            this.selectSearchEngineToolStripMenuItem,
+            this.zoomToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(23, 20);
+            this.toolStripMenuItem1.Text = "⋮";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // allowHttpToolStripMenuItem
+            // 
+            this.allowHttpToolStripMenuItem.Name = "allowHttpToolStripMenuItem";
+            this.allowHttpToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.allowHttpToolStripMenuItem.Text = "Allow http";
+            this.allowHttpToolStripMenuItem.Click += new System.EventHandler(this.allowHttpToolStripMenuItem_Click);
+            // 
+            // selectSearchEngineToolStripMenuItem
+            // 
+            this.selectSearchEngineToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.googleToolStripMenuItem,
+            this.duckDuckGoToolStripMenuItem,
+            this.bingToolStripMenuItem});
+            this.selectSearchEngineToolStripMenuItem.Name = "selectSearchEngineToolStripMenuItem";
+            this.selectSearchEngineToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.selectSearchEngineToolStripMenuItem.Text = "Select Search Engine";
+            this.selectSearchEngineToolStripMenuItem.Click += new System.EventHandler(this.selectSearchEngineToolStripMenuItem_Click);
+            // 
+            // googleToolStripMenuItem
+            // 
+            this.googleToolStripMenuItem.Name = "googleToolStripMenuItem";
+            this.googleToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.googleToolStripMenuItem.Text = "Google";
+            this.googleToolStripMenuItem.Click += new System.EventHandler(this.googleToolStripMenuItem_Click);
+            // 
+            // duckDuckGoToolStripMenuItem
+            // 
+            this.duckDuckGoToolStripMenuItem.Name = "duckDuckGoToolStripMenuItem";
+            this.duckDuckGoToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.duckDuckGoToolStripMenuItem.Text = "DuckDuckGo";
+            this.duckDuckGoToolStripMenuItem.Click += new System.EventHandler(this.duckDuckGoToolStripMenuItem_Click);
+            // 
+            // bingToolStripMenuItem
+            // 
+            this.bingToolStripMenuItem.Name = "bingToolStripMenuItem";
+            this.bingToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.bingToolStripMenuItem.Text = "Bing";
+            this.bingToolStripMenuItem.Click += new System.EventHandler(this.bingToolStripMenuItem_Click);
+            // 
+            // zoomToolStripMenuItem
+            // 
+            this.zoomToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoomInToolStripMenuItem,
+            this.zoomOutToolStripMenuItem,
+            this.toolStripMenuItem3});
+            this.zoomToolStripMenuItem.Name = "zoomToolStripMenuItem";
+            this.zoomToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.zoomToolStripMenuItem.Text = "Zoom";
+            // 
+            // zoomInToolStripMenuItem
+            // 
+            this.zoomInToolStripMenuItem.Name = "zoomInToolStripMenuItem";
+            this.zoomInToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zoomInToolStripMenuItem.Text = "Zoom in";
+            this.zoomInToolStripMenuItem.Click += new System.EventHandler(this.zoomInToolStripMenuItem_Click);
+            // 
+            // zoomOutToolStripMenuItem
+            // 
+            this.zoomOutToolStripMenuItem.Name = "zoomOutToolStripMenuItem";
+            this.zoomOutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zoomOutToolStripMenuItem.Text = "Zoom Out";
+            this.zoomOutToolStripMenuItem.Click += new System.EventHandler(this.zoomOutToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem3.Text = "100%";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(96, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(805, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "https://";
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // go
+            // 
+            this.go.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.go.Location = new System.Drawing.Point(907, 9);
+            this.go.MaximumSize = new System.Drawing.Size(34, 23);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(34, 23);
+            this.go.TabIndex = 2;
+            this.go.Text = "⤨";
+            this.go.UseVisualStyleBackColor = true;
+            this.go.Click += new System.EventHandler(this.go_Click);
+            // 
+            // webViewMain
+            // 
+            this.webViewMain.AllowExternalDrop = true;
+            this.webViewMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webViewMain.CreationProperties = null;
+            this.webViewMain.DefaultBackgroundColor = System.Drawing.Color.Black;
+            this.webViewMain.Location = new System.Drawing.Point(-3, 38);
+            this.webViewMain.Name = "webViewMain";
+            this.webViewMain.Size = new System.Drawing.Size(991, 726);
+            this.webViewMain.TabIndex = 3;
+            this.webViewMain.ZoomFactor = 1D;
+            this.webViewMain.CoreWebView2InitializationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs>(this.webViewMain_CoreWebView2InitializationCompleted);
+            this.webViewMain.NavigationStarting += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs>(this.webViewMain_NavigationStarting);
+            this.webViewMain.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webViewMain_NavigationCompleted);
+            this.webViewMain.Click += new System.EventHandler(this.webViewMain_Click);
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(12, 8);
+            this.back.MaximumSize = new System.Drawing.Size(22, 23);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(22, 23);
+            this.back.TabIndex = 4;
+            this.back.Text = "<";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(40, 8);
+            this.refresh.MaximumSize = new System.Drawing.Size(22, 23);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(22, 23);
+            this.refresh.TabIndex = 5;
+            this.refresh.Text = "↻";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // forward
+            // 
+            this.forward.Location = new System.Drawing.Point(68, 8);
+            this.forward.MaximumSize = new System.Drawing.Size(22, 23);
+            this.forward.Name = "forward";
+            this.forward.Size = new System.Drawing.Size(22, 23);
+            this.forward.TabIndex = 6;
+            this.forward.Text = ">";
+            this.forward.UseVisualStyleBackColor = true;
+            this.forward.Click += new System.EventHandler(this.forward_Click);
+            // 
+            // Reload_Anim_label
+            // 
+            this.Reload_Anim_label.AutoSize = true;
+            this.Reload_Anim_label.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Reload_Anim_label.ForeColor = System.Drawing.Color.Coral;
+            this.Reload_Anim_label.Location = new System.Drawing.Point(44, 13);
+            this.Reload_Anim_label.MaximumSize = new System.Drawing.Size(22, 23);
+            this.Reload_Anim_label.Name = "Reload_Anim_label";
+            this.Reload_Anim_label.Size = new System.Drawing.Size(18, 15);
+            this.Reload_Anim_label.TabIndex = 7;
+            this.Reload_Anim_label.Text = "↻";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.Reload_Anim_label);
+            this.Controls.Add(this.forward);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.webViewMain);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "JadeGo";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webViewMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button go;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webViewMain;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.Button forward;
+        private System.Windows.Forms.Label Reload_Anim_label;
+        private System.Windows.Forms.ToolStripMenuItem allowHttpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectSearchEngineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem googleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem duckDuckGoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomOutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+    }
+}
+
---
+++ JadeGo/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+namespace JadeGo
+{
+    public partial class Form1 : Form
+    {
+        private bool allowHttpPages = false;
+        private enum SearchEngine
+        {
+            Google,
+            Bing,
+            DuckDuckGo
+        }
+        private SearchEngine selectedSearchEngine = SearchEngine.Google;
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string name, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        public string path;
+
+        public void ReadWriteINIfile(string inipath)
+        {
+            path = inipath;
+        }
+        public void WriteINI(string name, string key, string value)
+        {
+            WritePrivateProfileString(name, key, value, this.path);
+        }
+        public string ReadINI(string name, string key)
+        {
+            StringBuilder sb = new StringBuilder(255);
+            int ini = GetPrivateProfileString(name, key, "", sb, 255, this.path);
+            return sb.ToString();
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            
+            InitializeAsync();
+
+            ReadWriteINIfile("./config.ini");
+
+            // Get The saved search engine config
+            string engineFromIni = ReadINI("SearchEngineConfig", "engine") == "" ? "Google" : ReadINI("SearchEngineConfig", "engine");
+
+            webViewMain.NavigationStarting += EnsureHttps;
+
+            Reload_Anim_label.Hide();
+
+            selectedSearchEngine = (SearchEngine)Enum.Parse(typeof(SearchEngine), engineFromIni);
+            selectSearchEngineToolStripMenuItem.Text = "Search Engine ("+engineFromIni+")";
+            toolStripMenuItem3.Text = (webViewMain.ZoomFactor * 100).ToString();
+        }
+
+
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webViewMain.Size = this.ClientSize - new System.Drawing.Size(webViewMain.Location);
+            //goButton.Left = this.ClientSize.Width - goButton.Width;
+            //addressBar.Width = goButton.Left - addressBar.Left;
+        }
+
+        async void InitializeAsync()
+        {
+            await webViewMain.EnsureCoreWebView2Async(null);
+            webViewMain.CoreWebView2.WebMessageReceived += UpdateAddressBar;
+
+            await webViewMain.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.postMessage(window.document.URL);");
+            //await webViewMain.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.addEventListener(\'message\', event => alert(event.data));");
+        }
+
+        void UpdateAddressBar(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            String uri = args.TryGetWebMessageAsString();
+            textBox1.Text = uri;
+            webViewMain.CoreWebView2.PostWebMessageAsString(uri);
+        }
+
+        public void Navigate(string url)
+        {
+            //MessageBox.Show("GO to " + url);
+            if (webViewMain != null && webViewMain.CoreWebView2 != null)
+            {
+                var rawUrl = url;
+                Uri uri = null;
+
+                if (Uri.IsWellFormedUriString(rawUrl, UriKind.Absolute))
+                {
+                    uri = new Uri(rawUrl);
+                }
+                else if (!rawUrl.Contains(" ") && rawUrl.Contains("."))
+                {
+                    // An invalid URI contains a dot and no spaces, try tacking https://
+                    uri = new Uri("https://" + rawUrl);
+                }
+                else
+                {
+                    // Otherwise treat it as a web search.
+                    if (selectedSearchEngine == SearchEngine.Bing)
+                    {
+                        uri = new Uri("https://bing.com/search?q=" +
+                        String.Join("+", Uri.EscapeDataString(rawUrl).Split(new string[] { "%20" }, StringSplitOptions.RemoveEmptyEntries)));
+                    } else if (selectedSearchEngine == SearchEngine.DuckDuckGo)
+                    {
+                        uri = new Uri("https://duckduckgo.com/?q=" +
+                        String.Join("+", Uri.EscapeDataString(rawUrl).Split(new string[] { "%20" }, StringSplitOptions.RemoveEmptyEntries)));
+                    } else
+                    {
+                        uri = new Uri("https://google.com/search?q=" +
+                        String.Join("+", Uri.EscapeDataString(rawUrl).Split(new string[] { "%20" }, StringSplitOptions.RemoveEmptyEntries)));
+                    }
+                    
+                    
+                }
+                
+                //MessageBox.Show("GO to " + uri.ToString());
+                if (FixEnsureHttps(uri.ToString()))
+                {
+                    webViewMain.CoreWebView2.Navigate(uri.ToString());
+                } else
+                {
+                    webViewMain.CoreWebView2.ExecuteScriptAsync($"alert('{url} is not an url')");
+
+                }
+                
+            }
+        }
+
+        public void NavigateBack()
+        {
+            if (webViewMain.CoreWebView2.CanGoBack)
+            {
+                webViewMain.CoreWebView2.GoBack();
+            }
+        }
+
+        public void NavigateForward()
+        {
+            if (webViewMain.CoreWebView2.CanGoForward)
+            {
+                webViewMain.CoreWebView2.GoForward();
+            }
+        }
+
+        public void RefreshView()
+        {
+            webViewMain.CoreWebView2.Reload();
+        }
+
+        void EnsureHttps(object sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            String uri = args.Uri;
+            if (!uri.StartsWith("https://"))
+            {
+                if (!allowHttpPages)
+                {
+                    webViewMain.CoreWebView2.ExecuteScriptAsync($"alert('{uri} is not safe, try an https link')");
+                    args.Cancel = true;
+                }
+                
+            }
+        }
+
+        bool FixEnsureHttps(string url)
+        {
+            if (url.StartsWith("https://") || url.StartsWith("http://")) {
+                return true;
+            }
+            return false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("JadeGo\nby Harri Ahola\n2022.");
+        }
+
+        private void go_Click(object sender, EventArgs e)
+        {
+            Navigate(textBox1.Text);
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            NavigateBack();
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            RefreshView();
+        }
+
+        private void forward_Click(object sender, EventArgs e)
+        {
+            NavigateForward();
+        }
+
+        private void webViewMain_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true; //remove the ding sound
+                Navigate(textBox1.Text);
+
+            }
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true; //remove the ding sound
+            }
+        }
+
+
+
+        private void webViewMain_NavigationStarting(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+            refresh.Hide();
+            Reload_Anim_label.Show();
+        }
+
+        private void webViewMain_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            Reload_Anim_label.Hide();
+            refresh.Show();
+            
+        }
+
+        private void webViewMain_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            //MessageBox.Show("DEBUG: the webview is ready!");
+        }
+
+        private void allowHttpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (allowHttpPages)
+            {
+                allowHttpToolStripMenuItem.Text = "Allow Http";
+                allowHttpPages = false;
+                allowHttpToolStripMenuItem.ForeColor = Color.DarkGreen;
+            } else
+            {
+                allowHttpPages = true;
+                allowHttpToolStripMenuItem.Text = "Disallow http";
+                allowHttpToolStripMenuItem.ForeColor = Color.Red;
+            }
+        }
+
+        private void googleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            selectedSearchEngine = SearchEngine.Google;
+            selectSearchEngineToolStripMenuItem.Text = "Search Engine (Google)";
+            WriteINI("SearchEngineConfig", "engine", "Google");
+        }
+
+        private void duckDuckGoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            selectedSearchEngine = SearchEngine.DuckDuckGo;
+            selectSearchEngineToolStripMenuItem.Text = "Search Engine (DuckDuckGo)";
+            WriteINI("SearchEngineConfig", "engine", "DuckDuckGo");
+        }
+
+        private void bingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            selectedSearchEngine = SearchEngine.Bing;
+            selectSearchEngineToolStripMenuItem.Text = "Search Engine (Bing)";
+            WriteINI("SearchEngineConfig", "engine", "Bing");
+        }
+
+        private void selectSearchEngineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zoomInToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webViewMain.ZoomFactor += 0.1;
+            toolStripMenuItem3.Text = (webViewMain.ZoomFactor * 100).ToString();
+        }
+
+        private void zoomOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webViewMain.ZoomFactor -= 0.1;
+            toolStripMenuItem3.Text = (webViewMain.ZoomFactor * 100).ToString();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
